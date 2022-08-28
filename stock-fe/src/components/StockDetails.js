1258b8f3da75c7532f7e6a805a21a75439e10c9a
Nellielin <nellllen0402@gmail.com>
2022-08-28 10:28:34 +0800

    123

--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -1,11 +1,21 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const StockDetails = () => {
   const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+
+  // 把網址上的 :stockId 拿出來
+  const { stockId } = useParams();
+  console.log('StockDetail - stockId', stockId);
+  // TODO: 去後端撈資料
+  // TODO: 1. axios.get -> 在哪個 useEffect 裡做？
+  // TODO: 2. setData
 
   return (
     <div>
       {error && <div>{error}</div>}
+      {/* TODO: 3. 在畫面上 render 資料, data.map */}
       <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
         <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：</h2>
         <h2 className="text-2xl font-bold mb-2 text-gray-800">成交金額：</h2>
